8cb6c1d4c0b5ce4feb387cddf025a921b7ea665e
Amanda <amandadgsales123@gmail.com>
2025-06-24 02:02:18 -0300

    renamed:    projectFinal/tela_risoto/prato_risoto.css -> css/prato_risoto.css 	modified:   project.php

--- project.php
+++ project.php
@@ -45,7 +45,7 @@
 
 
         <div class="item">
-            <a href="projectFinal/tela_strogonoff/prato_strogonoff.php">
+            <a href="prato_strogonoff.php">
                 <img src="imagens/strogonoff_cogumelos.png" alt="Strogonoff">
             </a>
             <div class="item-name">Strogonoff de Cogumelos<br>R$ 40.90</div>
