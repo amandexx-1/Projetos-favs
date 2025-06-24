3e4db18a2711c1f6758931be2015c375cd22ec1f
Amanda <amandadgsales123@gmail.com>
2025-06-24 00:34:09 -0300

    renamed:    projectFinal/tela_strogonoff/prato_strogonoff.css -> css/prato_strogonoff.css 	modified:   projectFinal/tela_strogonoff/prato_strogonoff.php

--- projectFinal/tela_strogonoff/prato_strogonoff.php
+++ projectFinal/tela_strogonoff/prato_strogonoff.php
@@ -6,13 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Strogonoff de Cogumelos</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="projectFinal/tela_strogonoff/prato_strogonoff.css">
+    <link rel="stylesheet" href="css/prato_strogonoff.css">
 </head>
 
 
 <body>
     <div class="container">
-        <a href="project.php">&lt;</a>
+        <a href="/project.php">&lt;</a>
         <h2>Strogonoff de Cogumelos</h2>
 
 
