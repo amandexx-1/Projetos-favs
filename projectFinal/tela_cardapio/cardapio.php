aebcb9e585369b5b7f4674f26f1664b1d60b0f05
Amanda <amandadgsales123@gmail.com>
2025-06-23 22:53:09 -0300

    modified:   projectFinal/tela_cardapio/cardapio.php

--- projectFinal/tela_cardapio/cardapio.php
+++ projectFinal/tela_cardapio/cardapio.php
@@ -20,8 +20,8 @@
 
 
         <div class="item">
-            <a href="projectFinal/tela_strogonoff/prato_strogonoff.php">
-                <img src="imagens/strogonoff_cogumelos.png" alt="Strogonoff">
+            <a href="/projectFinal/tela_strogonoff/prato_strogonoff.php">
+                <img src="/imagens/strogonoff_cogumelos.png" alt="Strogonoff">
             </a>
             <div class="item-name">Strogonoff de Cogumelos<br>R$ 40.90</div>
             <button onclick="addToCart(this)">Adicionar</button>
